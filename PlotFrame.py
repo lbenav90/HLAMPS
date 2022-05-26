5d6025da06e7edb8326db235c9bb2887269de0c1
lbenav90 <leandronbenavides@gmail.com>
2022-05-26 20:20:22 -0300

    Added comments and docstrings

--- PlotFrame.py
+++ PlotFrame.py
@@ -2,19 +2,21 @@ from tkinter import Tk, Frame
 from PltFigure import PltFigure
 
 class PlotFrame(Frame):
-    def __init__(self, master: Tk):
-        super().__init__(master)
-        self.master = master
-        self.config({'width': int(self.master.winfo_width() * 0.55),
-                     'height': int(self.master.winfo_height() * 0.65)})
+    ''' Contains the matplotlib plot. All actions defered to PltFigure class. '''
+    def __init__(self, window: Tk):
+        super().__init__(window)
+        self.window = window
+        self.config({'width': int(self.window.winfo_width() * 0.55),
+                     'height': int(self.window.winfo_height() * 0.65)})
         
         self.grid(column = 0, row = 0, 
                   rowspan = 2, sticky = 'news')
         
         self.update_idletasks()
-        self.figure = PltFigure(self, master)
+        self.figure = PltFigure(self, window)
     
     def clean(self):
+        ''' Clear widget elements and reinitialize the PltFigure element '''
         for widget in self.winfo_children():
             widget.destroy()
-        self.figure = PltFigure(self, self.master)
+        self.figure = PltFigure(self, self.window)

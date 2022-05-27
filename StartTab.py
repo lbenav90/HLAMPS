da3af14bc0307387ad6f77ac46f52f17342b6cb9
lbenav90 <leandronbenavides@gmail.com>
2022-05-26 21:05:59 -0300

    Added comments and docstrings

--- StartTab.py
+++ StartTab.py
@@ -1,35 +1,42 @@
 from settings import COLORS
-from tkinter import Button
-from tkinter.ttk import Frame
+from tkinter import Tk, Button
+from tkinter.ttk import Frame, Notebook
 
 class StartTab(Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self.window = master.master # GUI class instance
+    ''' Start tab is an almost empty tab '''
+    def __init__(self, notebook: Notebook):
+        super().__init__(notebook)
+        self.notebook = notebook
+        self.window = notebook.window # GUI class instance
         self.resetButton = Button(self, text = 'Restore program settings', 
-                                     command = lambda: [self.master.master.resetGUI(), 
-                                                        self.master.master.legFrame.updateLegend()]
-                                     ).grid(column = 3, row = 0, sticky = 'w')
+                                  command = lambda: [self.window.resetGUI(), 
+                                                     self.window.legFrame.updateLegend()]
+                                  ).grid(column = 3, row = 0, sticky = 'w')
     
     def clearTab(self):
+        ''' Noting to clear '''
         pass
     
     def discardChanges(self):
+        ''' No unsaved changes possible '''
         return True
         
-    def displaySpectra(self, window = None):
+    def displaySpectra(self, window: Tk = None):
+        ''' Display current average spectra with the Tab specific plots. '''
         if window is None:
-            window = self.master.master
+            window = self.window
             
         if window.maps.isEmpty():
             return 1
         
+        # Get current average spectra
         window.getAverageSpectra()
         
+        # Reinitialize plot
         plot = window.plotFrame.figure.reInitPlot()
         
         for col, spectra in window.averages.enumerate():  
+            # Plot all average spectra
             plot.plot(spectra[1][0],spectra[1][1], 
                       color = COLORS[col % len(COLORS)])
 
@@ -40,6 +47,6 @@ class StartTab(Frame):
         
         return 0
 
-    def handleMouseEvent(self, x, y):
-        #do nothing
+    def handleMouseEvent(self, x: float, y: float):
+        ''' No mouse event handling needed in this Tab '''
         return 0

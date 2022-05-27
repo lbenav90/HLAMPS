770a9629d7aabec8a22290dd7568f8f2d031338d
lbenav90 <leandronbenavides@gmail.com>
2022-05-27 19:17:22 -0300

    Added doctrings

--- Tabs.py
+++ Tabs.py
@@ -5,17 +5,19 @@ from SubtractTab import SubtractTab
 from FitTab import FitTab
 from dataclasses import dataclass, field
 
-
 @dataclass
 class Tabs:
+    ''' Data class containing all the Tabs in the notebook '''
     startTab: StartTab
     shiftTab: ShiftTab
     cutTab: CutTab
     subtractTab: SubtractTab
     fitTab: FitTab
+    ''' Initialize with an empty dictionary'''
     tabDict: dict = field(default_factory= lambda: ({}))
     
     def __post_init__(self):
+        ''' Populate tabDict with the Tabs after class initialization'''
         self.tabDict['Start'] = self.startTab
         self.tabDict['Shift spectra'] = self.shiftTab
         self.tabDict['Cut spectra'] = self.cutTab
@@ -23,4 +25,5 @@ class Tabs:
         self.tabDict['Fit spectra'] = self.fitTab
     
     def __iter__(self):
+        ''' For iteration '''
         return iter(self.tabDict.values())
